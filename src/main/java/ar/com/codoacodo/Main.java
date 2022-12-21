72b6ab35e9f561119a07770e408a024ae5ca4ce5
sarisaade <demabri@gmail.com>
2022-12-21 13:31:53 -0300

    first commit create maven project hola mundo

---
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -0,0 +1,12 @@
+package ar.com.codoacodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hola mundo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
